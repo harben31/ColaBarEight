1590b309489e31b23281db0eb4fc7911e6b74533
Benjamin Harris <harben31@gmail.com>
2022-04-15 08:03:37 -0700

    routes controllers

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const userControllers = require('../../controllers/userControllers');
+const userController = require('../../controllers/userController');
 const db = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.route('/')
-    .get(withAuth, userControllers.findAllUsers)
+    .get(withAuth, userController.findAllUsers);
+
+router.route('/username/')
+    .get(withAuth, userController.findUserByUsername);
+
+// router.route('/userId/:id')
+//     .get(withAuth, userController.findFriendById)
+//     .delete(userController.deleteUser);
+
+
 
 module.exports = router;
